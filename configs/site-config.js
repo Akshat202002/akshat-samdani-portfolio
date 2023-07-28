91a6f6be2c1e45a436e65db2a2fbd3224d77cca7
Akshat <akshatsamdani23@gmail.com>
2023-07-29 00:07:37 +0530

    Initial commit

---
+++ configs/site-config.js
@@ -0,0 +1,55 @@
+import {
+  FaGithub,
+  FaDev,
+  FaLinkedin,
+  FaQuora,
+  FaTwitter
+} from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+
+const siteConfig = {
+  copyright: `Copyright © ${new Date().getFullYear()} Akshat Samdani. All Rights Reserved.`,
+  author: {
+    name: "Akshat Samdani",
+    accounts: [
+      {
+        url: "https://github.com/Akshat202002/",
+        label: "Github Account",
+        type: "gray",
+        icon: <FaGithub />
+      },
+      {
+        url: "https://twitter.com/akshat_samdani",
+        label: "Twitter Account",
+        type: "twitter",
+        icon: <FaTwitter />
+      },
+      {
+        url: "https://dev.to/akshat202002",
+        label: "Dev Account",
+        type: "gray",
+        icon: <FaDev />
+      },
+      {
+        url: "https://www.linkedin.com/in/akshat-samdani/",
+        label: "LinkedIn Account",
+        type: "linkedin",
+        icon: <FaLinkedin />
+      },
+      // {
+      //   url: "",
+      //   label: "Quora Account",
+      //   type: "red",
+      //   icon: <FaQuora />
+      // },
+      {
+        url: "mailto:akshatsamdani23@gmail.com",
+        label: "Mail Akshat",
+        type: "gray",
+        icon: <FiMail />
+      }
+    ]
+  }
+};
+
+export default siteConfig;
---
+++ data/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * LINKS and USERNAMES
+ */
+export const GITHUB_API_URL = 'https://api.github.com';
+export const GITHUB_USERNAME = 'akshat202002';
+export const GITHUB_REPO = 'myportfolio';
+// TODO: add repo name
+export const DEVTO_API_URL = 'https://dev.to/api';
+
+/**
+ * SEO constants
+ */
+// TODO: add logo url
+export const SEO_IMAGE = 'https://i.imgur.com/SZ2yU0O.png';
